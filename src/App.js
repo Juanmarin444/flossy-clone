e73f223423425496bbf853e1eafce93d0708c19d
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2022-01-13 17:15:35 -0800

    initial layout for landing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import TopBanner from './components/TopBanner'
+
 import './App.css';
+import smile from './images/smile.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <TopBanner />
+      <div className='subBanner'>
+        <div className='mainContainer'>
+          <div className='bannerGrid'>
+            <div className='contentContainer'>
+              <div className='divblock9'>
+                <h1 className='heading'>
+                  <a className='logo'>
+                    <span class="logoText">Flossy</span>
+                  </a>
+                  <br/>
+                  For Patients Without
+                  <br/>
+                  Dental Insurance
+                </h1>
+                <ul className="list">
+
+                  <li className="listItem">Save up to 50% on&nbsp;Dental Treatments</li>
+                  <li className="listItem"></li>
+                  <li className="listItem">Membership is <span className="hero-span"><strong>FREE</strong></span><br/></li>
+                  <li className="listItem">Top-Rated Dentists</li>
+                  <li className="listItem"></li>
+                  
+                </ul>
+                <div className="div-flex">
+
+                  <div class="app-store-link-wrapper">
+                    <a href="" class="orange-button">Find A Dentist Near You</a>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
 
---
+++ src/components/TopBanner/index.js
@@ -0,0 +1,9 @@
+const TopBanner = () => {
+  return (
+    <div className="topBanner">
+      <p className="topBannerText">CALL 480-907-0632 TO BOOK AN APPOINTMENT</p>
+    </div>
+  );
+}
+
+export default TopBanner;
